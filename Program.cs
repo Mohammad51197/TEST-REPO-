543332b2f3232aacdc24a35eab5064b841d9880c
Zandar Beker (1000126240) <BEKEZA1@student.op.ac.nz>
2024-05-14 13:51:24 +1200

    Added my Name to Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello! My Name is Zandar :D");
+
+            Console.ReadLine();
         }
     }
 }
